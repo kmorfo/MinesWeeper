74bce88ffb9a11abea7f3ba5bcaf7318ed67296e
Raúl Luján Colilla <mcdrokz@gmail.com>
2024-01-03 17:15:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+<p align="center">
+  <a href="https://rlujancreations.es/" target="blank"><img src="https://rlujancreations.es/assets/img/logo%20rlujan%20SF.png" width="200" alt="RLujanCreations Logo" /></a>
+</p>
+
+# MinesWeeper
+This is my version of the classic game minesweeper developed in kotlin
---
+++ app/src/main/java/es/rlujancreations/minesweeper/MainActivity.kt
@@ -0,0 +1,46 @@
+package es.rlujancreations.minesweeper
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import es.rlujancreations.minesweeper.ui.theme.MinesWeeperTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MinesWeeperTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MinesWeeperTheme {
+        Greeting("Android")
+    }
+}
