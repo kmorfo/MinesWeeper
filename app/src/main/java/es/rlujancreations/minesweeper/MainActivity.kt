f71b138cc2fe1ada2e1a3ae784e9a81ffc65ac3f
Raúl Luján Colilla <mcdrokz@gmail.com>
2024-01-03 17:20:00 +0100

    Added App Icon

--- app/src/main/java/es/rlujancreations/minesweeper/MainActivity.kt
+++ app/src/main/java/es/rlujancreations/minesweeper/MainActivity.kt
@@ -22,25 +22,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MinesWeeperTheme {
-        Greeting("Android")
     }
 }
